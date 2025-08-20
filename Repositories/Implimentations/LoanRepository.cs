b6fb4a5f0412e6aaa4d77f5de7392655910164af
jlassi mazen <jlmazen1073@gmail.com>
2025-08-20 20:33:26 +0100

    deleting a loan +changing the availabilty + conditions

--- Repositories/Implimentations/LoanRepository.cs
+++ Repositories/Implimentations/LoanRepository.cs
@@ -2,10 +2,10 @@
 using Microsoft.EntityFrameworkCore;
 using bookFlow.Models;
 using bookFlow.Repositories.Interfaces;
-using System;
 using bookFlow.Data;
+using bookFlow.Enum;
 
-namespace bookFlow.Repositories.Implimentations
+namespace bookFlow.Repositories.Implementations
 {
     public class LoanRepository : ILoanRepository
     {
@@ -16,40 +16,75 @@ namespace bookFlow.Repositories.Implimentations
             _context = context;
         }
 
-
         public async Task AddAsync(Loan loan)
         {
             await _context.Loans.AddAsync(loan);
         }
-
 
         public void Delete(Loan entity)
         {
             _context.Loans.Remove(entity);
         }
 
+        public async Task DeleteAsync(Loan loan)
+        {
+            _context.Loans.Remove(loan);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<bool> DeleteLoanIfEnCoursAsync(Guid loanId)
+        {
+            var loan = await _context.Loans
+                .Include(l => l.Book)
+                .FirstOrDefaultAsync(l => l.Id == loanId);
+
+            if (loan == null || loan.Status != LoanStatus.EN_COURS)
+                return false;
+
+            // Set book back to available
+            if (loan.Book != null)
+                loan.Book.IsAvailable = true;
+
+            _context.Loans.Remove(loan);
+
+            // Tell EF explicitly that the book entity has changed
+            _context.Books.Update(loan.Book);
+
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
         public async Task<IEnumerable<Loan>> FindAsync(Expression<Func<Loan, bool>> predicate)
         {
-            return await _context.Loans.Include(l => l.Book).Include(l => l.User).Where(predicate).ToListAsync();
+            return await _context.Loans
+                .Include(l => l.Book)
+                .Include(l => l.User)
+                .Where(predicate)
+                .ToListAsync();
         }
 
         public async Task<IEnumerable<Loan>> GetAllAsync()
         {
-            return await _context.Loans.Include(l => l.Book).Include(l => l.User).ToListAsync();
+            return await _context.Loans
+                .Include(l => l.Book)
+                .Include(l => l.User)
+                .ToListAsync();
         }
 
         public async Task<Loan?> GetByIdAsync(Guid id)
         {
-            return await _context.Loans.FindAsync(id);
+            return await _context.Loans
+                .Include(l => l.Book)
+                .Include(l => l.User)
+                .FirstOrDefaultAsync(l => l.Id == id);
         }
-
 
         public async Task<Loan?> GetByIsbnAndUserAsync(string isbn, Guid userId)
         {
             return await _context.Loans
                 .Include(l => l.Book)
                 .Include(l => l.User)
-                .FirstOrDefaultAsync(l => l.Book.ISBN == isbn && l.UserId == userId && l.Status == Enum.LoanStatus.EN_COURS);
+                .FirstOrDefaultAsync(l => l.Book.ISBN == isbn && l.UserId == userId && l.Status == LoanStatus.EN_COURS);
         }
 
         public async Task<bool> SaveChangesAsync()
@@ -60,6 +95,38 @@ namespace bookFlow.Repositories.Implimentations
         public void Update(Loan entity)
         {
             _context.Loans.Update(entity);
+        }
+
+        // Additional useful methods you might want to add:
+
+        public async Task<IEnumerable<Loan>> GetLoansByUserIdAsync(Guid userId)
+        {
+            return await _context.Loans
+                .Include(l => l.Book)
+                .Where(l => l.UserId == userId)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Loan>> GetLoansByBookIdAsync(Guid bookId)
+        {
+            return await _context.Loans
+                .Include(l => l.User)
+                .Where(l => l.BookId == bookId)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Loan>> GetLoansByStatusAsync(LoanStatus status)
+        {
+            return await _context.Loans
+                .Include(l => l.Book)
+                .Include(l => l.User)
+                .Where(l => l.Status == status)
+                .ToListAsync();
+        }
+
+        public async Task<bool> ExistsAsync(Guid id)
+        {
+            return await _context.Loans.AnyAsync(l => l.Id == id);
         }
     }
 }
